3a00a4b581026d36a4a43890487889488dda2d50
Hubert Marzec <h.marzec@gmail.com>
2025-06-19 22:04:25 +0200

    wip

--- tests/e2e/test_api_endpoints.py
+++ tests/e2e/test_api_endpoints.py
@@ -2,10 +2,14 @@ import pytest
 
 
 @pytest.mark.e2e
-def describe_e2e():
-    def will_auth_with_env(auth_config):
+def describe_request():
+    def will_auth_with_env(auth_config, record_property):
         client_id = auth_config["client_id"]
         secret_id = auth_config["secret_id"]
+
+        record_property("client_id", client_id)
+        record_property("secret_id", secret_id)
+
         # Your test logic
         assert client_id is not None
         assert secret_id is not None
